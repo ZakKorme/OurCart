8d8ed946fecea3c851e3bfa0504b25b5326aa808
Zak Korme <zakariya.korme@gmail.com>
2021-05-29 12:20:15 -0400

    profile picture

--- app/containers/Account/Account.js
+++ app/containers/Account/Account.js
@@ -1,12 +1,154 @@
 import React from "react";
-import { Text, View, Container } from "native-base";
-import AppHeader from "../../components/Header/Header";
+import {
+  Avatar,
+  Title,
+  Caption,
+  Text,
+  TouchableRipple,
+} from "react-native-paper";
+import { StyleSheet, Button, View, SafeAreaView } from "react-native";
+import { Container } from "native-base";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function Account() {
   return (
-    <Container>
-      <AppHeader title={"Account"} />
-      <Text>Account</Text>
-    </Container>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.userInfoSection}>
+        <View style={{ flexDirection: "row", marginTop: 15 }}>
+          <Avatar.Image
+            source={require("../../assets/721px-Tomato_je.jpeg")}
+            size={80}
+          />
+          <View style={{ marginLeft: 20 }}>
+            <Title
+              style={[
+                styles.title,
+                {
+                  marginTop: 15,
+                  marginBottom: 5,
+                },
+              ]}
+            >
+              John Doe
+            </Title>
+            <Caption style={styles.caption}>user_id: @j_doe</Caption>
+          </View>
+        </View>
+        <View
+          style={[
+            styles.userInfoSection,
+            {
+              marginTop: 20,
+            },
+          ]}
+        >
+          <View style={styles.row}>
+            <Icon name="map-marker-radius" color="#777777" size={20} />
+            <Text style={{ color: "#777777", marginLeft: 20 }}>Severn, MD</Text>
+          </View>
+          <View style={styles.row}>
+            <Icon name="phone" color="#777777" size={20} />
+            <Text style={{ color: "#777777", marginLeft: 20 }}>
+              (240)-XXX-XXXX
+            </Text>
+          </View>
+          <View style={styles.row}>
+            <Icon name="email" color="#777777" size={20} />
+            <Text style={{ color: "#777777", marginLeft: 20 }}>
+              john_doe@gmail.com
+            </Text>
+          </View>
+        </View>
+        <View style={styles.infoBoxWrapper}>
+          <View
+            style={[
+              styles.infoBox,
+              {
+                borderRightColor: "#dddddd",
+                borderRightWidth: 1,
+              },
+            ]}
+          >
+            <Title>45</Title>
+            <Caption>Saved Recipes</Caption>
+          </View>
+          <View style={styles.infoBox}>
+            <Title>10</Title>
+            <Caption>Grocery Entries</Caption>
+          </View>
+        </View>
+        <View style={styles.menuWrapper}>
+          <TouchableRipple onPress={() => {}}>
+            <View style={styles.menuItem}>
+              <Icon name="heart-outline" color="green" size={25} />
+              <Text style={styles.menuItemText}>Favorite Recipes</Text>
+            </View>
+          </TouchableRipple>
+          <TouchableRipple onPress={() => {}}>
+            <View style={styles.menuItem}>
+              <Icon name="cart-outline" color="green" size={25} />
+              <Text style={styles.menuItemText}>Grocery Items</Text>
+            </View>
+          </TouchableRipple>
+          <TouchableRipple onPress={() => {}}>
+            <View style={styles.menuItem}>
+              <Icon name="magnify-scan" color="green" size={25} />
+              <Text style={styles.menuItemText}>Remove Pantry Item</Text>
+            </View>
+          </TouchableRipple>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  userInfoSection: {
+    paddingHorizontal: 30,
+    marginBottom: 25,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+    fontWeight: "500",
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+  infoBoxWrapper: {
+    borderBottomColor: "#dddddd",
+    borderBottomWidth: 1,
+    borderTopColor: "#dddddd",
+    borderTopWidth: 1,
+    flexDirection: "row",
+    height: 100,
+  },
+  infoBox: {
+    width: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  menuWrapper: {
+    marginTop: 10,
+  },
+  menuItem: {
+    flexDirection: "row",
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+  },
+  menuItemText: {
+    color: "#777777",
+    marginLeft: 20,
+    fontWeight: "600",
+    fontSize: 16,
+    lineHeight: 26,
+  },
+});
